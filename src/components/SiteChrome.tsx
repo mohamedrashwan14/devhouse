5f945cfa713fc7781926ebf4a27180a95a2e6476
Mohamed Rashwan <maemraa@gmail.com>
2026-09-07 16:15:51 +0300

    Add missing SiteChrome component

---
+++ src/components/SiteChrome.tsx
@@ -0,0 +1,11 @@
+import Footer from '@/components/Footer'
+import { WhatsAppButton } from '@/components/WhatsAppButton'
+
+export function SiteChrome() {
+  return (
+    <>
+      <Footer />
+      <WhatsAppButton />
+    </>
+  )
+}
